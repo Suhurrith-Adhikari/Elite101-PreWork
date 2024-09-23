52e795cdbb635681ea245ea034aee81975353140
Suhurrith Adhikari <suhurrith.adhikari@gmail.com>
2024-09-23 19:56:18 +0000

    Adding code to the Elite101ChatbotProject

--- Elite101ChatbotProject.py
+++ Elite101ChatbotProject.py
@@ -1,2 +1,13 @@
+print("Hello and welcome to Suhurrith's Chatbot!!")
 
-print("Hello Suhurrith")
+print("")
+
+name = input("What is your name?: ")
+
+age = int(input("Hello " + name + ", what is your age?: "))
+
+print("I can almost reminsice about my days when i was " + str(age) + ", Mr. " + name)
+
+print("Please choose from the following options: ")
+
+
